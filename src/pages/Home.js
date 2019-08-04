9190c91b2cce807b728178679c46941718192b67
Samuel Beard <sbeard@brightsource.co.uk>
2019-08-04 23:26:39 +0100

    kinda works

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,36 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 
 const Home = () => {
-    const [isHappy, setHappy] = useState()
+    const [isLoading, setLoading] = useState(false)
+    const [data, setData] = useState({})
 
-    return (
-        <p>Home</p>
-    )
+    useEffect(() => {
+        setLoading(true)
+        axios
+            .post(`${process.env.REACT_APP_BASE_API_ROUTE}/graphql`, {
+                query: `
+                mutation {
+                    sign_up(userInput: {email: "samisgay", password: "asnrg34", userName: "mamamamfjfjfj"}) {
+                    userName
+                    token
+                    tokenExpiry
+                }
+            }
+            `,
+            })
+            .then(res => {
+                console.log(res)
+                return res.data.data.sign_up
+            })
+            .then(json => {
+                setData(json)
+                console.log(json.token)
+            })
+            setLoading(false)
+    }, [])
+
+    return isLoading ? <p>Loading</p> : <p>{data.userName}</p>
 }
 
 export default Home
