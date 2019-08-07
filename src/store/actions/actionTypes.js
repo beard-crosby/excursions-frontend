a88fc460f9fc39672ee46492682247098e331619
Maximilian118 <maxcrosby118@gmail.com>
2019-08-07 23:29:53 +0100

    ActionTypes

---
+++ src/store/actions/actionTypes.js
@@ -0,0 +1,10 @@
+// ActionTypes are used to cary data from a component to the reducer.
+// It's usually the case that you'd want to do something with the data before it gets to the reducer however.
+// Hence actionCreators. 
+
+// misc
+export const LOADING = 'LOADING'
+
+// auth
+export const REQUEST_FAIL = 'REQUEST_FAIL'
+export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS'
