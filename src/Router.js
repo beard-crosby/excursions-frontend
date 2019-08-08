c54a07c1a4d4fc183a5ca007ea10da7051a532c7
Maximilian118 <maxcrosby118@gmail.com>
2019-08-08 15:58:35 +0100

    Router Fix

--- src/Router.js
+++ src/Router.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { connect } from "react-redux"
 import { Route, Switch } from "react-router-dom"
 import PrivateRoute from "./components/PrivateRoute"
 
@@ -6,11 +7,19 @@ import Home from "./pages/Home"
 import Dashboard from "./pages/Dashboard"
 import NotFound from "./pages/error/NotFound"
 
-const Router = props => 
+const Router = ({ state }) => 
     <Switch>
-        <PrivateRoute exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/" component={Home} /> 
+        {state.token ? 
+            <PrivateRoute exact path="/" component={Dashboard} />  
+            :
+            <Route exact path="/" component={Home} /> 
+        }
+        
         <Route component={NotFound} />
     </Switch>
 
-export default Router
+export default connect(state => ({
+    state: {
+        token: state.token
+    },
+}))(Router)
