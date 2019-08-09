8c8cefaf65b198317521020e7b59fac0017285d5
Maximilian118 <maxcrosby118@gmail.com>
2019-08-09 19:00:05 +0100

    Cleanup & .env.dev port correction

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,13 +1,9 @@
-import React, { useEffect } from "react"
+import React from "react"
 import { connect } from "react-redux"
 import Layout from '../components/Layout'
 import Spinner from "../components/UI/Spinner/Spinner"
 
 const Dashboard = ({ state, dispatch }) => {
-    useEffect(() => {
-        // we send our requests here via 'dispatch'
-    }, [])
-
     return (
         <Layout >
             {state.loading ? <Spinner /> : <h1>Dashboard</h1>}
