f3ec1272c059c36256904e1a7182b170e8bbe831
Samuel Beard <sbeard@brightsource.co.uk>
2019-08-16 13:24:15 +0100

    add email label checks

--- cypress/integration/e2e/home.spec.js
+++ cypress/integration/e2e/home.spec.js
@@ -7,9 +7,10 @@ context("Home Page", () => {
 
     describe("The signup form", ()=> {
         it("displays error when invalid email in typed", () => {
+            cy.get('#email-label').contains('Email')
             cy.get('#email').type('notanemailaddress')
             cy.get('#username').type('validusername')
-            // cy.get('label.Input_Invalid__3aeyU').contains('Please enter a valid Email.')
+            cy.get('#email-label').contains('Please enter a valid Email.')
             cy.get('#submit-signup').should('be.disabled')
             cy.get('#full-name').should('exist').type('John Doe')
             cy.get('#password').type('password')
